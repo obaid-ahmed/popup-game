07a68fa28463ee17fdd0c7d7650d0f366382556e
obaid-ahmed <81504716+obaid-ahmed@users.noreply.github.com>
2021-09-19 20:33:57 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5HU4fPw6X9B6DHBdwMO75oC7o8UVZ0wY",
+    authDomain: "popup-81224.firebaseapp.com",
+    projectId: "popup-81224",
+    storageBucket: "popup-81224.appspot.com",
+    messagingSenderId: "383335063899",
+    appId: "1:383335063899:web:ebed10e9709077e1b57a7e",
+    measurementId: "G-5LPJHCCFL2"
+  };
+
+  
